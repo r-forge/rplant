910d73bee01f0189dc4b974b424097646056499b
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-30 15:22:50 +0000

    added basic RunMuscle function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/RunMuscle.R
@@ -0,0 +1,16 @@
+RunMuscle <- function(user.name, token, fasta.file, job.name=NULL, tnrs=FALSE) {
+  if (is.null(job.name))
+    job.name <- paste(user.name, "MUSCLEviaAPI", sep="")
+  if (!is.SeqFastadna(fasta.file[[1]]))
+    stop("fasta.file is not in fasta formatting")
+  # if (tnrs) {
+  #  speciesNames <- attr(fasta.file, "name")  
+  #  TNRSspeciesNames <- ResolveNames(speciesNames, maxPerCall=100, verbose=F)
+  #  attr(fasta.file, "name") <- TNRSspeciesNames
+  #  CompareTNRS(speciesNames, TNRSspeciesNames, verbose=F)
+  # }
+  myJob<-SubmitJob(user.name, token, application="muscle-ranger-2.0", path=fasta.file, job.name=job.name, nprocs="1")
+  return(myJob)
+}
+
+RunMuscle(user.name, token, "/bbanbury/rplant/landplant.fasta")
