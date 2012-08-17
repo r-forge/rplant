c6017afaa2fb156c8c26b2411addecb7d62d373a
bbanbury (Barb Banbury) <bbanbury@utk.edu>
2012-08-17 22:46:09 +0000

    also working on the GetCitations function to make it more fluid and save bib files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@146 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rPlant.R
+++ pkg/R/rPlant.R
@@ -379,14 +379,13 @@ CompareNames <- function(old.names, new.names, verbose=TRUE) {
 GetCitations <- function(application, save=FALSE, file="rPlant.bib") {
   # Returns citations for programs and analyses
   # So we are going to make the library of citations.  I would suggest we make a bibtex entry alongside these entries, someone might want a different citation method.
-  # save and file currently do not work.
   citations<-c()
   if (application == "muscle-ranger-2.0") {
   	citations <-append(citations, ref("10.1093/nar/gkh340"))
   	citations <-append(citations, ref("10.1186/1471-2105-5-113"))
   }
-  if (application == "") {
-  	
+  if (application != "muscle-ranger-2.0") {
+  	print("working on more applications")
   }
   if(save)
     write.bibtex(citations, file=file, append=T) 
