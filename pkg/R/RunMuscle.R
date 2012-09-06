c2d2a9eeaa5b337b615d7ca24dc6c8dd2f2259af
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-06 16:15:13 +0000

    added other api versions (those that work) to RunMuscle wrapper
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@160 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunMuscle.R
+++ pkg/R/RunMuscle.R
@@ -1,13 +1,7 @@
-RunMuscle <- function(user.name, token, DE.file.name="", DE.file.path="", job.name=NULL, tnrs=FALSE) {
+RunMuscle <- function(user.name, token, DE.file.name="", DE.file.path="", job.name=NULL, version="muscle-ranger-2.0") {
   if (is.null(job.name))
     job.name <- paste(user.name, "MUSCLEviaAPI", sep="")
-  # if (tnrs) {
-  #  speciesNames <- attr(fasta.file, "name")  
-  #  TNRSspeciesNames <- ResolveNames(speciesNames, maxPerCall=100, verbose=F)
-  #  attr(fasta.file, "name") <- TNRSspeciesNames
-  #  CompareTNRS(speciesNames, TNRSspeciesNames, verbose=F)
-  # }
-  myJob<-SubmitJob(user.name, token, application="muscle-ranger-2.0", 
+  myJob<-SubmitJob(user.name, token, application=version, 
                    DE.file.name=DE.file.name, DE.file.path=DE.file.path, 
                    job.name=job.name, nprocs="1")
   return(myJob)
