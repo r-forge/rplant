a280864e7baa95bf56d67f9a0511057c9cf7aec4
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2012-09-10 21:01:52 +0000

    add wrapper
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@166 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/RunMafft.R
@@ -0,0 +1,9 @@
+RunMafft <- function(user.name, token, DE.file.name="", DE.file.path="", job.name=NULL, version=c("mafft-lonestar-6.864")) {
+  version <- match.arg(version)
+  if (is.null(job.name))
+    job.name <- paste(user.name,"_", version, "viaAPI", sep="")
+  myJob<-SubmitJob(user.name, token, application=version, 
+                   DE.file.name=DE.file.name, DE.file.path=DE.file.path, 
+                   job.name=job.name, nprocs="1")
+  return(myJob)
+}
