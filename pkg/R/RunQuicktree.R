ed3e10371b0fbd4c61e221d9c7879e9e1dd0db15
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-10 21:04:04 +0000

    add wrapper
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@171 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/RunQuicktree.R
@@ -0,0 +1,9 @@
+RunQuicktree <- function(user.name, token, DE.file.name="", DE.file.path="", job.name=NULL, version=c("quicktree-dm-lonestar-1.1", "quicktree-tree-lonestar-1.1")) {
+  version <- match.arg(version)
+  if (is.null(job.name))
+    job.name <- paste(user.name,"_", version, "viaAPI", sep="")
+  myJob<-SubmitJob(user.name, token, application=version, 
+                   DE.file.name=DE.file.name, DE.file.path=DE.file.path, 
+                   job.name=job.name, nprocs="1")
+  return(myJob)
+}
