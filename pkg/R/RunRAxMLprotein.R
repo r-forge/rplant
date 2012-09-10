fdf2a75029e1f7e09377729d8f4fbcec8cb20f80
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-10 21:04:35 +0000

    add wrapper
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@173 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/RunRAxMLprotein.R
@@ -0,0 +1,15 @@
+RunRAxMLprotein <- function(user.name, token, DE.file.name="", DE.file.path="", job.name=NULL, model=c("PROTCAT", "PROTGAMMA", "PROTCAT.GAMMA", "PROTGAMMAI", "PROTCAT.GAMMAI"), bootstrap=NULL, numcat=25, nprocs=1) {
+  model <- match.arg(model)
+  if (is.null(job.name))
+    job.name <- paste(user.name, "_RAxMLprotein_", model, "viaAPI", sep="")
+
+#initialize arguments
+  args <- paste("&arguments=-m", model)
+  if (!is.null(bootstrap)) 
+    args <- append(args, c("-b", bootstrap))
+  args <- append(args, c("-c", numcat))
+  args <- paste(args, collapse=" ")  # make a single statement
+ 
+  # Submit
+  SubmitJob(user.name, token, "raxml-lonestar-7.2.8", DE.file.name, DE.file.path, job.name, nprocs, args)
+}
