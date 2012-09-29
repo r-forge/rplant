4e41f7f98a5f2842408eef705d10547088e26c86
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-29 00:09:02 +0000

    added help file info and cleaned up raxml wrappers
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@183 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunRAxMLdna.R
+++ pkg/R/RunRAxMLdna.R
@@ -1,9 +1,10 @@
-RunRAxMLdna <- function(user.name, token, DE.file.name="", DE.file.path="", job.name=NULL, model=c("GTRCAT", "GTRGAMMA", "GTRCAT.GAMMA", "GTRGAMMAI", "GTRCAT.GAMMAI"), bootstrap=NULL, numcat=25, nprocs=1) {
-  model <- match.arg(model)
+RunRAxMLdna <- function(user.name, token, DE.file.name="", 
+                        DE.file.path="", job.name=NULL, model="GTRCAT", 
+                        bootstrap=NULL, numcat=25, nprocs=1) {
   if (is.null(job.name))
     job.name <- paste(user.name, "_RAxMLdna_", model, "viaAPI", sep="")
 
-#initialize arguments
+  #initialize arguments
   args <- paste("&arguments=-m", model)
   if (!is.null(bootstrap)) 
     args <- append(args, c("-b", bootstrap))
@@ -11,5 +12,6 @@ RunRAxMLdna <- function(user.name, token, DE.file.name="", DE.file.path="", job.
   args <- paste(args, collapse=" ")  # make a single statement
  
   # Submit
-  SubmitJob(user.name, token, "raxml-lonestar-7.2.8", DE.file.name, DE.file.path, job.name, nprocs, args)
+  SubmitJob(user.name, token, "raxml-lonestar-7.2.8", DE.file.name, 
+            DE.file.path, job.name, nprocs, args)
 }
