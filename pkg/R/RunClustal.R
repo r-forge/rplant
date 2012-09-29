83ac8d27e6307561bbefd21fb430a28d930fea6c
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-29 00:31:03 +0000

    added Rd for RunClustal and cleaned up a bit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@184 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunClustal.R
+++ pkg/R/RunClustal.R
@@ -1,5 +1,4 @@
-RunClustal <- function(user.name, token, DE.file.name="", DE.file.path="", job.name=NULL, version=c("ClustalW2-2.1", "clustalw2-lonestar-2.1", "clustalw-ranger-1.0")) {
-  version <- match.arg(version)
+RunClustal <- function(user.name, token, DE.file.name="", DE.file.path="", job.name=NULL, version="ClustalW2-2.1") {
   if (is.null(job.name))
     job.name <- paste(user.name,"_", version, "viaAPI", sep="")
   myJob<-SubmitJob(user.name, token, application=version, 
