a5937e0de8d467b1d2f3621b3f67b578e6f71f8a
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-02-08 17:46:39 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@232 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunMuscle.R
+++ pkg/R/RunMuscle.R
@@ -3,6 +3,6 @@ RunMuscle <- function(user.name, token, DE.file.name="", DE.file.path="", job.na
     job.name <- paste(user.name,"_", version, "viaAPI", sep="")
   myJob<-SubmitJob(user.name, token, application=version, 
                    DE.file.name=DE.file.name, DE.file.path=DE.file.path, 
-                   job.name=job.name, nprocs="1")
+                   job.name=job.name, nprocs=1)
   return(myJob)
 }
