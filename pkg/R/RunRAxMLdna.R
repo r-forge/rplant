ef8de71f6edfbcb51e3d9eef0c2cc36b1e55c429
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-05-13 21:07:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunRAxMLdna.R
+++ pkg/R/RunRAxMLdna.R
@@ -1,9 +1,9 @@
 RunRAxMLdna <- function(user.name, token, DE.file.name="", DE.file.path="", 
-                        outputFileName=NULL, model="GTRCAT", bootstrap=NULL, 
+                        job.name=NULL, model="GTRCAT", bootstrap=NULL, 
                         algorithm="d", multipleModelFileName=NULL, 
                         numcat=25, nprocs=2) {
-  if (is.null(outputFileName)) {
-    outputFileName <- paste(user.name, "_RAxMLdna_", model, "viaAPI", sep="")
+  if (is.null(job.name)) {
+    job.name <- paste(user.name, "_RAxMLdna_", model, "viaAPI", sep="")
   }
 
   #initialize arguments
@@ -17,7 +17,7 @@ RunRAxMLdna <- function(user.name, token, DE.file.name="", DE.file.path="",
   #args <- append(args, c("-#", numberOfRuns))
   args <- append(args, c("-c", numcat))
   args <- append(args, c("-s", DE.file.name))
-  args <- append(args, c("-n", outputFileName))
+  args <- append(args, c("-n", job.name))
   if(!is.null(multipleModelFileName)) {
     args <- append(args, c("-q", multipleModelFileName))
   }
@@ -25,5 +25,5 @@ RunRAxMLdna <- function(user.name, token, DE.file.name="", DE.file.path="",
  
   # Submit
   SubmitJob(user.name, token, "raxml-lonestar-7.2.8u1", DE.file.name, 
-            DE.file.path, job.name=outputFileName, nprocs, args)
+            DE.file.path, job.name=job.name, nprocs, args)
 }
