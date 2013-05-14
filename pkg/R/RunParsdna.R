b3bccba823fc0baad209b0feb36f6439de0398b6
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-05-14 22:57:55 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@274 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunParsdna.R
+++ pkg/R/RunParsdna.R
@@ -1,10 +1,16 @@
 RunParsdna <- function(user.name, token, DE.file.name="", DE.file.path="", 
-                       job.name=NULL) {
+                       job.name=NULL, nprocs=1) {
+
+  App <- GetAppInfo(user.name, token, application)[[2]]
+  input.list <- vector("list",1)
+  input.list[[1]] <- App[,2][1]
+
   if (is.null(job.name))
-    job.name <- paste(user.name, "_Parsdna_viaAPI", sep="")
-  myJob<-SubmitJob(user.name, token, 
-                   application="phylip-dna-parsimony-lonestar-3.69u1", 
-                   DE.file.name=DE.file.name, DE.file.path=DE.file.path, 
-                   job.name=job.name, nprocs=1)
+    job.name <- paste(user.name,"_Parsdna_viaR", sep="")
+
+  myJob<-SubmitJob(user.name, token, application="phylip-dna-parsimony-lonestar-3.69u1", 
+                   DE.file.list=list(DE.file.name), DE.file.path=DE.file.path, 
+                   input.list=input.list, job.name=job.name, nprocs=nprocs)
+
   return(myJob)
 }
