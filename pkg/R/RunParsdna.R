48dd89e0e88669905f7d407dffa3648b56410ced
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-05-14 23:27:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@284 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunParsdna.R
+++ pkg/R/RunParsdna.R
@@ -1,7 +1,7 @@
 RunParsdna <- function(user.name, token, DE.file.name="", DE.file.path="", 
                        job.name=NULL, nprocs=1) {
 
-  App <- GetAppInfo(user.name, token, application)[[2]]
+  App <- GetAppInfo(user.name, token, "phylip-dna-parsimony-lonestar-3.69u1")[[2]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
