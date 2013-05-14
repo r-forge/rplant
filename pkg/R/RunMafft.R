735c4e2a7cf7aadb1f082c4cf2d1bfff18aa28bb
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-14 23:26:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@282 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunMafft.R
+++ pkg/R/RunMafft.R
@@ -1,7 +1,7 @@
 RunMafft <- function(user.name, token, DE.file.name="", DE.file.path="", 
                      job.name=NULL, nprocs=1) {
 
-  App <- GetAppInfo(user.name, token, application)[[2]]
+  App <- GetAppInfo(user.name, token, "mafft-lonestar-6.864u1")[[2]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
