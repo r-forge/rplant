fdf921d2bcf57d7132838e24e93cbc95656c494b
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-05-15 18:24:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@293 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunMafft.R
+++ pkg/R/RunMafft.R
@@ -1,14 +1,14 @@
-RunMafft <- function(user.name, token, DE.file.name="", DE.file.path="", 
-                     job.name=NULL, nprocs=1) {
+RunMafft <- function(user.name, token, DE.file.name, DE.file.path="", job.name=NULL, 
+                     nprocs=1, version="mafft-lonestar-6.864u1") {
 
-  App <- GetAppInfo(user.name, token, "mafft-lonestar-6.864u1")[[2]]
+  App <- GetAppInfo(user.name, token, version)[[2]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
   if (is.null(job.name))
-    job.name <- paste(user.name,"_Mafft_viaR", sep="")
+    job.name <- paste(user.name,"_",version,"_viaR", sep="")
 
-  myJob<-SubmitJob(user.name, token, application="mafft-lonestar-6.864u1", 
+  myJob<-SubmitJob(user.name, token, application=version, 
                    DE.file.list=list(DE.file.name), DE.file.path=DE.file.path, 
                    input.list=input.list, job.name=job.name, nprocs=nprocs)
 
