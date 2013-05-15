362e9d8b52c8fb6346527eae92d232c2155d1142
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-15 18:26:58 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@299 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunRAxMLprotein.R
+++ pkg/R/RunRAxMLprotein.R
@@ -1,11 +1,11 @@
 RunRAxMLprotein <- function(user.name, token, DE.file.name="", DE.file.path="", 
                             job.name=NULL, model="PROTCAT", bootstrap=NULL, 
-                            numcat=25, nprocs=2) {
+                            numcat=25, nprocs=2, version="raxml-lonestar-7.2.8u1") {
   if (is.null(job.name)){
     job.name <- paste(user.name, "_RAxMLprotein_", model, "_viaR", sep="")
   }
 
-  App <- GetAppInfo(user.name, token, "raxml-lonestar-7.2.8u1")[[2]]
+  App <- GetAppInfo(user.name, token, version)[[2]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
@@ -18,7 +18,7 @@ RunRAxMLprotein <- function(user.name, token, DE.file.name="", DE.file.path="",
   args <- paste(args, collapse=" ")  # make a single statement
  
   # Submit
-  myJob<-SubmitJob(user.name, token, application="raxml-lonestar-7.2.8u1", 
+  myJob<-SubmitJob(user.name, token, application=version, 
                    DE.file.list=list(DE.file.name), DE.file.path=DE.file.path, 
                    input.list=input.list, job.name=job.name, nprocs=nprocs)
 }
