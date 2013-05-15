59d90965c53de6dd81d98aaa738dd458612b4241
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-05-15 18:25:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@297 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunQuicktree.R
+++ pkg/R/RunQuicktree.R
@@ -1,14 +1,15 @@
-RunQuicktree <- function(user.name, token, DE.file.name="", DE.file.path="",
-                         job.name=NULL, nprocs=1) {
+RunQuicktree <- function(user.name, token, DE.file.name, DE.file.path="",
+                         job.name=NULL, nprocs=1, 
+                         version="quicktree-tree-lonestar-1.1u1") {
 
-  App <- GetAppInfo(user.name, token, "quicktree-tree-lonestar-1.1u1")[[2]]
+  App <- GetAppInfo(user.name, token, version)[[2]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
   if (is.null(job.name))
-    job.name <- paste(user.name,"_Quicktree_viaR", sep="")
+    job.name <- paste(user.name,"_",version,"_viaR", sep="")
 
-  myJob<-SubmitJob(user.name, token, application="quicktree-tree-lonestar-1.1u1", 
+  myJob<-SubmitJob(user.name, token, application=version, 
                    DE.file.list=list(DE.file.name), DE.file.path=DE.file.path, 
                    input.list=input.list, job.name=job.name, nprocs=nprocs)
 
