03749054389de13dd651dccc2457105509e0af36
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-17 04:59:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@372 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/RunFaST_LMM.R
@@ -0,0 +1,84 @@
+RunFaST_LMM <- function(user.name, token, input.file.list="", ALL.file.path="",
+                        sim.file.list=NULL, pheno.file.name=NULL, mpheno=1, nprocs=1,
+                        print.curl=FALSE, job.name=NULL, covar.file.name=NULL, 
+                        version="FaST-LMM-1.09", shared.user.name=NULL, args=NULL) {
+
+  input.len <- length(input.file.list)
+  input.list <- list()
+  if ((input.len) == 3){
+    ext1 <- unlist(strsplit(input.file.list[[1]], "\\."))[2]
+    ext2 <- unlist(strsplit(input.file.list[[2]], "\\."))[2]
+    ext3 <- unlist(strsplit(input.file.list[[3]], "\\."))[2]
+    input.type="B"
+    input.list[[1]] <- find.input(ext1)
+    input.list[[2]] <- find.input(ext2)
+    input.list[[3]] <- find.input(ext3)
+  } else {
+    ext1 <- unlist(strsplit(input.file.list[[1]], "\\."))[2]
+    ext2 <- unlist(strsplit(input.file.list[[2]], "\\."))[2]
+    input.list[[1]] <- find.input(ext1)
+    input.list[[2]] <- find.input(ext2)
+    if ((ext1 == "tfam" ) || (ext1 == "tped")) {
+      input.type="T"
+    } else {
+      input.type="R"
+    }
+  }
+
+  args <- c("arguments=",args)
+
+  if (input.type=="T"){
+    options <- list(c("T",TRUE))
+    if (!is.null(sim.file.list)){
+      input.file.list <- append(input.file.list,sim.file.list)
+      input.list <- append(input.list,c("Sim1","Sim2"))
+      options <- append(options,list(c("S",TRUE)))
+    }
+  } else if (input.type=="B") {
+    options <- list(c("B",TRUE))
+    if (!is.null(sim.file.list)){
+      input.file.list <- append(input.file.list,sim.file.list)
+      input.list <- append(input.list,c("Sim1","Sim2","Sim3"))
+      options <- append(options,list(c("S",TRUE)))
+    }
+  } else {
+    options <- NULL
+    if (!is.null(sim.file.list)){
+      input.file.list <- append(input.file.list,sim.file.list)
+      input.list <- append(input.list,c("Sim1","Sim2"))
+      options <- append(options,list(c("S",TRUE)))
+    }
+  }
+
+  if (is.null(job.name)){
+    job.name <- unlist(strsplit(input.file.list[[1]], "\\."))[1]
+
+  }
+
+  args <- append(args, c("-out",job.name))
+
+  if (!is.null(pheno.file.name)){
+    input.file.list <- append(input.file.list, pheno.file.name)
+    input.list <- append(input.list,"inputPHENO")
+    options <- append(options,list(c("P",TRUE),c("mpheno",mpheno)))
+  }
+
+  if (!is.null(covar.file.name)){
+    input.file.list <- append(input.file.list, covar.file.name)
+    input.list <- append(input.list,"inputCOVAR")
+    options <- append(options,list(c("C",TRUE)))
+  }
+
+  # make a single statement
+  args <- paste(args, collapse=" ") 
+
+  # Submit
+  myJob<-SubmitJob(user.name, token, application=version, options.list=options, 
+                   DE.file.list=input.file.list, DE.file.path=ALL.file.path,
+                   input.list=input.list, job.name=job.name, nprocs=nprocs, 
+                   print.curl=print.curl, shared.user.name=shared.user.name,
+                   args=args)
+
+  return(list(myJob,job.name))
+
+}
