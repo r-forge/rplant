593079cd737ccfe551d32be704af7538d25ac67b
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-17 04:59:29 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@373 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunPLINK.R
+++ pkg/R/RunPLINK.R
@@ -1,11 +1,27 @@
 RunPLINK <- function(user.name, token, DE.file.list="", DE.file.path="",
-                     input.list=list("inputMAP","inputPED"), input.type="R", 
                      job.name=NULL, association.method="--assoc", no.sex=TRUE,
-                     print.curl=FALSE, multi.adjust=TRUE, nprocs=1,
+                     print.curl=FALSE, multi.adjust=TRUE, nprocs=1, args=NULL,
                      version="plink-1.07", shared.user.name=NULL) {
-  
 
-  args <- paste("arguments=", association.method)
+  input.len <- length(DE.file.list)
+  input.list <- list()
+  if ((input.len) == 3){
+    ext1 <- unlist(strsplit(DE.file.list[[1]], "\\."))[2]
+    ext2 <- unlist(strsplit(DE.file.list[[2]], "\\."))[2]
+    ext3 <- unlist(strsplit(DE.file.list[[3]], "\\."))[2]
+    input.type="B"
+    input.list[[1]] <- find.input(ext1)
+    input.list[[2]] <- find.input(ext2)
+    input.list[[3]] <- find.input(ext3)
+  } else {
+    ext1 <- unlist(strsplit(DE.file.list[[1]], "\\."))[2]
+    ext2 <- unlist(strsplit(DE.file.list[[2]], "\\."))[2]
+    input.list[[1]] <- find.input(ext1)
+    input.list[[2]] <- find.input(ext2)
+    if ((ext1 == "tfam" ) || (ext1 == "tped")) {input.type="T"}
+  }  
+
+  args <- c(paste("arguments=", association.method), args)
 
   if (multi.adjust){args <- append(args, c("--adjust"))}
 
@@ -13,41 +29,47 @@ RunPLINK <- function(user.name, token, DE.file.list="", DE.file.path="",
 
   if (input.type=="T"){
     options <- list(c("T",TRUE))
-    BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-5)
-    EXT1 <- substr(DE.file.list[[1]],nchar(DE.file.list[[1]])-3,nchar(DE.file.list[[1]]))
-    BASE2 <- substr(DE.file.list[[2]],1,nchar(DE.file.list[[2]])-5)
-    if (EXT1 == "tfam"){
-      job.name <- paste(BASE1,"_",BASE2,"_", association.method, sep="")
-    } else {
-      job.name <- paste(BASE2,"_",BASE1,"_", association.method, sep="")
+    if (is.null(job.name)){
+      BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-5)
+      EXT1 <- substr(DE.file.list[[1]],nchar(DE.file.list[[1]])-3,nchar(DE.file.list[[1]]))
+      BASE2 <- substr(DE.file.list[[2]],1,nchar(DE.file.list[[2]])-5)
+      if (EXT1 == "tfam"){
+        job.name <- paste(BASE1,"_",BASE2,"_", association.method, sep="")
+      } else {
+        job.name <- paste(BASE2,"_",BASE1,"_", association.method, sep="")
+      }
     }
     args <- append(args, c("--out",job.name))
   } else if (input.type=="B") {
     options <- list(c("B",TRUE))
-    BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-4)
-    EXT1 <- substr(DE.file.list[[1]],nchar(DE.file.list[[1]])-2,nchar(DE.file.list[[1]]))
-    BASE2 <- substr(DE.file.list[[2]],1,nchar(DE.file.list[[2]])-4)
-    EXT2 <- substr(DE.file.list[[2]],nchar(DE.file.list[[2]])-2,nchar(DE.file.list[[1]]))
-    BASE3 <- substr(DE.file.list[[3]],1,nchar(DE.file.list[[3]])-4)
-    EXT3 <- substr(DE.file.list[[3]],nchar(DE.file.list[[3]])-2,nchar(DE.file.list[[1]]))
-    if (EXT1 == "bed" && EXT3 == "bam"){
-      job.name <- paste(BASE1, "_", BASE3, "_", BASE2, "_", association.method, sep="")
-    } else if (EXT1 == "bed" && EXT2 == "bam") {
-      job.name <- paste(BASE1, "_", BASE2, "_", BASE3, "_", association.method, sep="")
-    } else if (EXT2 == "bed" && EXT3 == "bam"){
-      job.name <- paste(BASE2, "_", BASE3, "_", BASE1, "_", association.method, sep="")
-    } else if (EXT3 == "bed" && EXT1 == "bam"){
-      job.name <- paste(BASE3, "_", BASE1, "_", BASE2, "_", association.method, sep="")
-    } else if (EXT2 == "bed" && EXT1 == "bam") {
-      job.name <- paste(BASE2, "_", BASE1, "_", BASE3, "_", association.method, sep="")
-    } else {
-      job.name <- paste(BASE3, "_", BASE2, "_", BASE1, "_", association.method, sep="")
+    if (is.null(job.name)){
+      BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-4)
+      EXT1 <- substr(DE.file.list[[1]],nchar(DE.file.list[[1]])-2,nchar(DE.file.list[[1]]))
+      BASE2 <- substr(DE.file.list[[2]],1,nchar(DE.file.list[[2]])-4)
+      EXT2 <- substr(DE.file.list[[2]],nchar(DE.file.list[[2]])-2,nchar(DE.file.list[[1]]))
+      BASE3 <- substr(DE.file.list[[3]],1,nchar(DE.file.list[[3]])-4)
+      EXT3 <- substr(DE.file.list[[3]],nchar(DE.file.list[[3]])-2,nchar(DE.file.list[[1]]))
+      if (EXT1 == "bed" && EXT3 == "bam"){
+        job.name <- paste(BASE1, "_", BASE3, "_", BASE2, "_", association.method, sep="")
+      } else if (EXT1 == "bed" && EXT2 == "bam") {
+        job.name <- paste(BASE1, "_", BASE2, "_", BASE3, "_", association.method, sep="")
+      } else if (EXT2 == "bed" && EXT3 == "bam"){
+        job.name <- paste(BASE2, "_", BASE3, "_", BASE1, "_", association.method, sep="")
+      } else if (EXT3 == "bed" && EXT1 == "bam"){
+        job.name <- paste(BASE3, "_", BASE1, "_", BASE2, "_", association.method, sep="")
+      } else if (EXT2 == "bed" && EXT1 == "bam") {
+        job.name <- paste(BASE2, "_", BASE1, "_", BASE3, "_", association.method, sep="")
+      } else {
+        job.name <- paste(BASE3, "_", BASE2, "_", BASE1, "_", association.method, sep="")
+      }
     }
     args <- append(args, c("--out",job.name))
   } else {
     options <- NULL
-    BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-4)
-    job.name <- paste(BASE1,"_", association.method, sep="")
+    if (is.null(job.name)){
+      BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-4)
+      job.name <- paste(BASE1,"_", association.method, sep="")
+    }
     args <- append(args, c("--out",job.name))
   }
 
