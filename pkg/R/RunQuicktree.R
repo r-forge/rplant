af230c2f936f34a3aa3b7cf1852598e429b3fcdf
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-05 04:25:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@390 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunQuicktree.R
+++ pkg/R/RunQuicktree.R
@@ -1,19 +1,40 @@
-RunQuicktree <- function(user.name, token, DE.file.name, DE.file.path="",
-                         job.name=NULL, nprocs=1, print.curl=FALSE,
-                         version="quicktree-tree-lonestar-1.1u1",
-                         shared.user.name=NULL) {
+RunQuicktree <- function(file.name, file.path="", job.name=NULL, UPGMA=FALSE,
+                         kimura=FALSE, boot=0, output.file.name=NULL,
+                         version="quicktree-tree-lonestar-1.1u1", 
+                         print.curl=FALSE, shared.user.name=NULL, 
+                         suppress.Warnings=FALSE) {
 
-  App <- GetAppInfo(user.name, token, version)[[2]]
+  if (UPGMA){
+    args="arguments=-upgma"
+  } else if (kimura){
+    args <- append(args,"-kimura")
+  } else if (boot>0){
+    args <- append(args,paste("-boot",boot))
+  } else if (!is.null(output.file.name)){
+    args <- append(args,paste("-out",output.file.name))
+  }
+
+  # make a single statement
+  args <- paste(args, collapse=" ") 
+
+  nprocs <- 1
+  App <- GetAppInfo(version)[[2]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
-  if (is.null(job.name))
-    job.name <- paste(user.name,"_",version,"_viaR", sep="")
+  if (is.null(job.name)){
+    job.name <- paste(rplant.env$user,"_",version,"_viaR", sep="")
+  }
 
-  myJob<-SubmitJob(user.name, token, application=version, 
-                   DE.file.list=list(DE.file.name), DE.file.path=DE.file.path, 
-                   input.list=input.list, job.name=job.name, nprocs=nprocs,
-                   print.curl=print.curl, shared.user.name=shared.user.name)
+  if (!is.null(args)){
+    args <- paste("arguments=,",args)
+  }
+
+  myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs,
+                   file.list=list(file.name), file.path=file.path, 
+                   input.list=input.list, suppress.Warnings=suppress.Warnings,
+                   print.curl=print.curl, shared.user.name=shared.user.name,
+                   args=args)
 
   return(myJob)
 }
