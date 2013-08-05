d40d1fb00a14e9f538eb180aa6a373988f20d205
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-08-05 04:25:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@391 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunRAxMLdna.R
+++ pkg/R/RunRAxMLdna.R
@@ -1,18 +1,21 @@
-RunRAxMLdna <- function(user.name, token, DE.file.name="", DE.file.path="", 
-                        job.name=NULL, model="GTRCAT", bootstrap=NULL, 
-                        algorithm="d", multipleModelFileName=NULL, 
-                        numcat=25, nprocs=2, version="raxml-lonestar-7.2.8u1",
-                        print.curl=FALSE, shared.user.name=NULL) {
+RunRAxMLdna <- function(file.name, file.path="", job.name=NULL,
+                        model="GTRCAT", bootstrap=NULL, algorithm="d",
+                        multipleModelFileName=NULL, args=NULL, numcat=25,
+                        nprocs=12, version="raxml-lonestar-7.2.8u1",
+                        print.curl=FALSE, shared.user.name=NULL,
+                        suppress.Warnings=FALSE) {
+
   if (is.null(job.name)){
-    job.name <- paste(user.name, "_RAxMLdna_", model, "_viaR", sep="")
+    job.name <- paste(rplant.env$user, "_RAxMLdna_", model, "_viaR", sep="")
   }
 
-  App <- GetAppInfo(user.name, token, version)[[2]]
+  App <- GetAppInfo(version)[[2]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
   #initialize arguments
-  args <- paste("arguments=-m", model)
+  args <- paste("arguments=", args)
+  args <- append(args, c("-m", model))
   #args <- append(args, c("-T", numberOfThreads))
   if (!is.null(bootstrap)) {
     args <- append(args, c("-b", bootstrap))
@@ -21,7 +24,7 @@ RunRAxMLdna <- function(user.name, token, DE.file.name="", DE.file.path="",
   args <- append(args, c("-p", floor(runif(1, 1, 10^6))))
   #args <- append(args, c("-#", numberOfRuns))
   args <- append(args, c("-c", numcat))
-  args <- append(args, c("-s", DE.file.name))
+  args <- append(args, c("-s", file.name))
   args <- append(args, c("-n", job.name))
   if(!is.null(multipleModelFileName)) {
     args <- append(args, c("-q", multipleModelFileName))
@@ -29,10 +32,9 @@ RunRAxMLdna <- function(user.name, token, DE.file.name="", DE.file.path="",
   args <- paste(args, collapse=" ")  # make a single statement
  
   # Submit
-  myJob<-SubmitJob(user.name, token, application=version, 
-                   DE.file.list=list(DE.file.name), DE.file.path=DE.file.path, 
-                   input.list=input.list, job.name=job.name, nprocs=nprocs,
+  myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs,
+                   file.list=list(file.name), file.path=file.path, 
+                   input.list=input.list, suppress.Warnings=suppress.Warnings,
                    print.curl=print.curl, shared.user.name=shared.user.name,
                    args=args)
-
 }
