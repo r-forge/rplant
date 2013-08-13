b8cb4dd45c8f5a3fd9b7fe4c2ed9c27f15e7ad80
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-13 21:46:27 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@478 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunParsprotein.R
+++ pkg/R/RunParsprotein.R
@@ -4,15 +4,15 @@ RunParsprotein <- function(file.name, file.path="", job.name=NULL,
                            suppress.Warnings=FALSE) {
 
   nprocs <- 1
-  App <- GetAppInfo(version)[[2]]
+  App <- GetAppInfo(version)[[3]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
   if (is.null(job.name))
-    job.name <- paste(user.name,"_",version,"_viaR", sep="")
+    job.name <- paste(rplant.env$user,"_",version,"_viaR", sep="")
 
   myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs,
-                   DE.file.list=list(file.name), file.path=file.path, 
+                   file.list=list(file.name), file.path=file.path, 
                    input.list=input.list, suppress.Warnings=suppress.Warnings,
                    print.curl=print.curl, shared.username=shared.username)
 
