6717ea140ddcf74590ca9c62d5d84e74704ee928
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-08-13 21:46:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@476 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunPLINK.R
+++ pkg/R/RunPLINK.R
@@ -1,23 +1,23 @@
-RunPLINK <- function(DE.file.list="", DE.file.path="", job.name=NULL,
+RunPLINK <- function(file.list="", file.path="", job.name=NULL,
                      association.method="--assoc", no.sex=TRUE, args=NULL,
                      print.curl=FALSE, multi.adjust=TRUE, version="plink-1.07", 
                      shared.username=NULL, suppress.Warnings=FALSE) {
 
   nprocs <- 1
   private.APP <- TRUE
-  input.len <- length(DE.file.list)
+  input.len <- length(file.list)
   input.list <- list()
   if ((input.len) == 3){
-    ext1 <- unlist(strsplit(DE.file.list[[1]], "\\."))[2]
-    ext2 <- unlist(strsplit(DE.file.list[[2]], "\\."))[2]
-    ext3 <- unlist(strsplit(DE.file.list[[3]], "\\."))[2]
+    ext1 <- unlist(strsplit(file.list[[1]], "\\."))[2]
+    ext2 <- unlist(strsplit(file.list[[2]], "\\."))[2]
+    ext3 <- unlist(strsplit(file.list[[3]], "\\."))[2]
     input.type="B"
     input.list[[1]] <- find.input(ext1)
     input.list[[2]] <- find.input(ext2)
     input.list[[3]] <- find.input(ext3)
   } else {
-    ext1 <- unlist(strsplit(DE.file.list[[1]], "\\."))[2]
-    ext2 <- unlist(strsplit(DE.file.list[[2]], "\\."))[2]
+    ext1 <- unlist(strsplit(file.list[[1]], "\\."))[2]
+    ext2 <- unlist(strsplit(file.list[[2]], "\\."))[2]
     input.list[[1]] <- find.input(ext1)
     input.list[[2]] <- find.input(ext2)
     if ((ext1 == "tfam" ) || (ext1 == "tped")) {input.type="T"}
@@ -32,9 +32,9 @@ RunPLINK <- function(DE.file.list="", DE.file.path="", job.name=NULL,
   if (input.type=="T"){
     options <- list(c("T",TRUE))
     if (is.null(job.name)){
-      BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-5)
-      EXT1 <- substr(DE.file.list[[1]],nchar(DE.file.list[[1]])-3,nchar(DE.file.list[[1]]))
-      BASE2 <- substr(DE.file.list[[2]],1,nchar(DE.file.list[[2]])-5)
+      BASE1 <- substr(file.list[[1]],1,nchar(file.list[[1]])-5)
+      EXT1 <- substr(file.list[[1]],nchar(file.list[[1]])-3,nchar(file.list[[1]]))
+      BASE2 <- substr(file.list[[2]],1,nchar(file.list[[2]])-5)
       if (EXT1 == "tfam"){
         job.name <- paste(BASE1,"_",BASE2,"_", association.method, sep="")
       } else {
@@ -45,12 +45,12 @@ RunPLINK <- function(DE.file.list="", DE.file.path="", job.name=NULL,
   } else if (input.type=="B") {
     options <- list(c("B",TRUE))
     if (is.null(job.name)){
-      BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-4)
-      EXT1 <- substr(DE.file.list[[1]],nchar(DE.file.list[[1]])-2,nchar(DE.file.list[[1]]))
-      BASE2 <- substr(DE.file.list[[2]],1,nchar(DE.file.list[[2]])-4)
-      EXT2 <- substr(DE.file.list[[2]],nchar(DE.file.list[[2]])-2,nchar(DE.file.list[[1]]))
-      BASE3 <- substr(DE.file.list[[3]],1,nchar(DE.file.list[[3]])-4)
-      EXT3 <- substr(DE.file.list[[3]],nchar(DE.file.list[[3]])-2,nchar(DE.file.list[[1]]))
+      BASE1 <- substr(file.list[[1]],1,nchar(file.list[[1]])-4)
+      EXT1 <- substr(file.list[[1]],nchar(file.list[[1]])-2,nchar(file.list[[1]]))
+      BASE2 <- substr(file.list[[2]],1,nchar(file.list[[2]])-4)
+      EXT2 <- substr(file.list[[2]],nchar(file.list[[2]])-2,nchar(file.list[[1]]))
+      BASE3 <- substr(file.list[[3]],1,nchar(file.list[[3]])-4)
+      EXT3 <- substr(file.list[[3]],nchar(file.list[[3]])-2,nchar(file.list[[1]]))
       if (EXT1 == "bed" && EXT3 == "bam"){
         job.name <- paste(BASE1, "_", BASE3, "_", BASE2, "_", association.method, sep="")
       } else if (EXT1 == "bed" && EXT2 == "bam") {
@@ -69,7 +69,7 @@ RunPLINK <- function(DE.file.list="", DE.file.path="", job.name=NULL,
   } else {
     options <- NULL
     if (is.null(job.name)){
-      BASE1 <- substr(DE.file.list[[1]],1,nchar(DE.file.list[[1]])-4)
+      BASE1 <- substr(file.list[[1]],1,nchar(file.list[[1]])-4)
       job.name <- paste(BASE1,"_", association.method, sep="")
     }
     args <- append(args, c("--out",job.name))
