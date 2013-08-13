edf3ae27c377209a6c5af2e86651b5e08011eb8a
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-08-13 21:46:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@479 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RunQuicktree.R
+++ pkg/R/RunQuicktree.R
@@ -1,24 +1,31 @@
 RunQuicktree <- function(file.name, file.path="", job.name=NULL, UPGMA=FALSE,
                          kimura=FALSE, boot=0, output.file.name=NULL,
-                         version="quicktree-tree-lonestar-1.1u1", 
+                         version="quicktree-tree-lonestar-1.1u2", 
                          print.curl=FALSE, shared.username=NULL, 
                          suppress.Warnings=FALSE) {
 
+  flag <- 0
   if (UPGMA){
     args=c("-upgma")
+    flag <- 1
   } else if (kimura){
     args <- append(args,"-kimura")
+    flag <- 1
   } else if (boot>0){
     args <- append(args,paste("-boot",boot))
+    flag <- 1
   } else if (!is.null(output.file.name)){
     args <- append(args,paste("-out",output.file.name))
+    flag <- 1
   }
 
   # make a single statement
-  args <- paste(args, collapse=" ") 
+  if (flag == 1){
+    args <- paste(args, collapse=" ")
+  }
 
   nprocs <- 1
-  App <- GetAppInfo(version)[[2]]
+  App <- GetAppInfo(version)[[3]]
   input.list <- vector("list",1)
   input.list[[1]] <- App[,2][1]
 
@@ -26,9 +33,13 @@ RunQuicktree <- function(file.name, file.path="", job.name=NULL, UPGMA=FALSE,
     job.name <- paste(rplant.env$user,"_",version,"_viaR", sep="")
   }
 
-  if (!is.null(args)){
+  if (flag == 1){
     args <- list("arguments", args)
+  } else {
+    args <- NULL
   }
+
+  Renew()
 
   myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs,
                    file.list=list(file.name), file.path=file.path, 
