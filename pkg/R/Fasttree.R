4c478c833830c41c4469a2150da5008ea4946bea
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-17 00:02:21 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@508 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/Fasttree.R
@@ -0,0 +1,25 @@
+RunFasttree <- function(file.name, file.path="", job.name=NULL, args=NULL,
+                        print.curl=FALSE, version="fasttreeDispatcher-1.0.0u1", 
+                        shared.username=NULL, suppress.Warnings=FALSE) {
+
+  nprocs <- 1
+  App <- GetAppInfo(version)[[3]]
+  input.list <- vector("list",1)
+  input.list[[1]] <- App[,2][1]
+
+  if (is.null(job.name)){
+    job.name <- paste(rplant.env$user,"_",version,"_viaR", sep="")
+  }
+
+  if (!is.null(args)){
+    args <- list(c("arguments",args))
+  }
+
+  myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs,
+                   file.list=list(file.name), file.path=file.path, 
+                   input.list=input.list, suppress.Warnings=suppress.Warnings,
+                   print.curl=print.curl, shared.username=shared.username,
+                   args.list=args)
+
+  return(myJob)
+}
