71699e001b20f7131081e3b82977113ab2bee295
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-20 20:34:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@554 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Fasttree.R
+++ pkg/R/Fasttree.R
@@ -1,6 +1,49 @@
-Fasttree <- function(file.name, file.path="", job.name=NULL, args=NULL,
+Fasttree <- function(file.name, file.path="", job.name=NULL, args=NULL, 
+                     type="DNA", model=NULL, gamma=FALSE, stat=FALSE,
                      print.curl=FALSE, version="fasttreeDispatcher-1.0.0u1", 
                      shared.username=NULL, suppress.Warnings=FALSE) {
+
+  type <- match.arg(type, c("DNA", "PROTEIN"))
+
+  if (type == "DNA"){
+
+    if (is.null(model)){
+      model="GTRCAT"
+    }
+
+    model <- match.arg(model, c("GTRCAT", "JCCAT"))
+
+    if (is.null(job.name)){
+      job.name <- paste(rplant.env$user, "_Fasttreedna_", model, "_viaR", sep="")
+    }
+
+  } else {
+
+    if (is.null(model)){
+      model="JTTCAT"
+    }
+
+    model <- match.arg(model, c("JTTCAT", "WAGCAT"))
+
+    if (is.null(job.name)){
+      job.name <- paste(rplant.env$user, "_Fastreeprotein_", model, "_viaR", sep="")
+    }
+  }
+
+  if (model == "GTRCAT"){
+    args <- append(args, "-gtr -nt")
+  } else if (aln.filetype == "WAGCAT"){
+    args <- append(args, "-wag")
+  }
+
+  if (gamma == TRUE) {
+    args <- append(args, "-gamma")
+  }
+
+  if (stat == TRUE) {
+    args <- append(args, "-log logfile")
+  }
+  args <- paste(args, collapse=" ")  # make a single statement
 
   nprocs <- 1
   App <- GetAppInfo(version)[[3]]
