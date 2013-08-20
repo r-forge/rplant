2df88626d52d1cb5310ed9cc2b75e5176237a2cd
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-20 20:35:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@556 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Muscle.R
+++ pkg/R/Muscle.R
@@ -1,12 +1,6 @@
 Muscle <- function(file.name, file.path="", job.name=NULL, args=NULL,
-                   version="muscle-lonestar-3.8.31u2", print.curl=FALSE,
+                   version="Muscle-3.8.32u1", print.curl=FALSE,
                    shared.username=NULL, suppress.Warnings=FALSE) {
-
-  if (version == "Muscle-3.8.32"){
-    private.APP = TRUE
-  } else {
-    private.APP = FALSE
-  }
 
   nprocs <- 1
   App <- GetAppInfo(version)[[3]]
@@ -16,13 +10,15 @@ Muscle <- function(file.name, file.path="", job.name=NULL, args=NULL,
   if (is.null(job.name))
     job.name <- paste(rplant.env$user,"_",version,"_viaR", sep="")
 
-  args <- list(c("arguments",paste("-msf -msfout msf.aln", args)))
+  if (!is.null(job.name)){
+    args <- list(c("arguments",args))
+  }
 
   myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs,
                    file.list=list(file.name), file.path=file.path, 
                    input.list=input.list, suppress.Warnings=suppress.Warnings,
                    print.curl=print.curl, shared.username=shared.username,
-                   args.list=args, private.APP=private.APP)
+                   args.list=args)
 
   return(myJob)
 }
