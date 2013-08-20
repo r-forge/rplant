930a0c1a15a5098787e143abbaba8be492287e97
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2013-08-20 20:34:45 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@553 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ClustalW.R
+++ pkg/R/ClustalW.R
@@ -1,6 +1,30 @@
-ClustalW <- function(file.name, file.path="", job.name=NULL, args=NULL,
-                     version="ClustalW2-2.1u1", print.curl=FALSE,
-                     shared.username=NULL, suppress.Warnings=FALSE) {
+ClustalW <- function(file.name, file.path="", type="DNA", job.name=NULL,
+                     version="ClustalW2-2.1u1", print.curl=FALSE,  args=NULL,
+                     aln.filetype="PHYLIP", shared.username=NULL,
+                     suppress.Warnings=FALSE) {
+
+  type <- match.arg(type, c("DNA", "PROTEIN"))
+
+  aln.filetype <- match.arg(aln.filetype, c("CLUSTAL", "PHYLIP", "NEXUS", "GCG", "GDE", "PIR"))
+
+  if (type == "DNA"){
+    args <- append(args, "-TYPE=DNA")
+  } else {
+    args <- append(args, "-TYPE=PROTEIN")
+  }
+
+  if (aln.filetype == "PHYLIP"){
+    args <- append(args, "-OUTPUT=PHYLIP")
+  } else if (aln.filetype == "NEXUS"){
+    args <- append(args, "-OUTPUT=NEXUS")
+  } else if (aln.filetype == "GCG"){
+    args <- append(args, "-OUTPUT=GCG")
+  } else if (aln.filetype == "GDE"){
+    args <- append(args, "-OUTPUT=GDE")
+  } else if (aln.filetype == "PIR"){
+    args <- append(args, "-OUTPUT=PIR")
+  }
+
   nprocs=1
   App <- GetAppInfo(version)[[3]]
   input.list <- vector("list",1)
@@ -9,9 +33,9 @@ ClustalW <- function(file.name, file.path="", job.name=NULL, args=NULL,
   if (is.null(job.name))
     job.name <- paste(rplant.env$user,"_",version,"_viaR", sep="")
 
-  if (!is.null(args)){
-    args <- list(c("arguments",args))
-  }
+  args <- paste(args, collapse=" ")  # make a single statement
+
+  args <- list(c("arguments",args))
 
   myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs, 
                    file.list=list(file.name), file.path=file.path, 
@@ -19,5 +43,6 @@ ClustalW <- function(file.name, file.path="", job.name=NULL, args=NULL,
                    print.curl=print.curl, shared.username=shared.username,
                    args.list=args)
 
+  cat("Result file: clustalw2.fa\n")
   return(myJob)
 }
