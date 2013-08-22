e15e46d807f4f9ff606d91cf9b550c95040e3b9e
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-22 04:17:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@576 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/PHYLIP.R
@@ -0,0 +1,27 @@
+PHYLIP <- function(file.name, file.path="", job.name=NULL, type="DNA",
+                   print.curl=FALSE, shared.username=NULL, 
+                   suppress.Warnings=FALSE) {
+
+  type <- match.arg(type, c("DNA", "PROTEIN"))
+
+  if (type == "DNA") {
+    version="phylip-dna-parsimony-lonestar-3.69u2"
+  } else {
+    version="phylip-protein-parsimony-lonestar-3.69u2"
+  }
+
+  nprocs <- 1
+  App <- GetAppInfo(version)[[3]]
+  input.list <- vector("list",1)
+  input.list[[1]] <- App[,2][1]
+
+  if (is.null(job.name))
+    job.name <- paste(rplant.env$user,"_",version,"_viaR", sep="")
+
+  myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs,
+                   file.list=list(file.name), file.path=file.path, 
+                   input.list=input.list, suppress.Warnings=suppress.Warnings,
+                   print.curl=print.curl, shared.username=shared.username)
+
+  return(myJob)
+}
