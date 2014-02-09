0aecf7b4669c0162964332980cb53aa0b89886e8
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2014-02-09 03:29:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@703 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/PHYLIP_Pars.R
+++ pkg/R/PHYLIP_Pars.R
@@ -1,6 +1,6 @@
 PHYLIP_Pars <- function(file.name, file.path="", job.name=NULL, type="DNA",
                         print.curl=FALSE, shared.username=NULL, 
-                        suppress.Warnings=FALSE) {
+                        suppress.Warnings=FALSE, email=TRUE) {
 
   type <- match.arg(type, c("DNA", "PROTEIN"))
 
@@ -19,7 +19,7 @@ PHYLIP_Pars <- function(file.name, file.path="", job.name=NULL, type="DNA",
     job.name <- paste(rplant.env$user,"_",version,"_viaR", sep="")
 
   myJob<-SubmitJob(application=version, job.name=job.name, nprocs=nprocs,
-                   file.list=list(file.name), file.path=file.path, 
+                   file.list=list(file.name), file.path=file.path, email=email,
                    input.list=input.list, suppress.Warnings=suppress.Warnings,
                    print.curl=print.curl, shared.username=shared.username)
 
