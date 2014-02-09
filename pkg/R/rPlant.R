f298560efa37b1b9fd52419d981f0192a88c611b
kamichels <kamichels@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-09 03:39:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@710 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rPlant.R
+++ pkg/R/rPlant.R
@@ -209,6 +209,35 @@ Error <- function(ERR){
   }
 }
 
+Wait <- function(job.id, minWaitsec, maxWaitsec, print=FALSE){
+  currentStatus= ''
+  currentWait = minWaitsec
+  while (( currentStatus != 'FAILED' ) && (currentStatus != 'ARCHIVING_FINISHED')) {
+    # cache the status from previous inquiry
+    oldStatus = currentStatus
+    currentStatus = CheckJobStatus( job.id )
+
+    if (currentStatus == oldStatus) {# status hasn't changed from last time we asked so
+      currentWait = currentWait * 1.10 # wait 10% longer to poll in the future
+
+      if (currentWait > maxWaitsec) {
+        currentWait = maxWaitsec # but don't wait too long
+      }
+    } else {
+      currentWait = minWaitsec # status changed so reset wait counter to min value
+    }
+    
+#   if (print == TRUE) {
+#     print(paste("Wait Time:", currentWait, "Status:", currentStatus))
+#   }
+    
+    Sys.sleep(currentWait) # sit idle for proscribed time. If you are using an event-based programming model, you could just schedule the next check currentWait sec in the future 
+  }
+
+  if (print == TRUE) {
+    print(paste("Job number: '", job.id, "' has status: ", currentStatus, sep=""))
+  }
+}
 # -- END -- #
 
 
