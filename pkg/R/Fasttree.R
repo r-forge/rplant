e8ddc3631d6af6292539e832407bdba19a9bf6ea
kamichels (Kurt Michels) <kamichels@math.arizona.edu>
2014-06-19 18:56:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rplant@797 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Fasttree.R
+++ pkg/R/Fasttree.R
@@ -3,6 +3,35 @@ Fasttree <- function(file.name, file.path="", job.name=NULL, args=NULL,
                      print.curl=FALSE, version="fasttreeDispatcher-1.0.0u1", 
                      shared.username=NULL, suppress.Warnings=FALSE, email=TRUE) {
 
+  # FastTree infers approximately-maximum-likelihood phylogenetic trees 
+  #   from alignments of nucleotide or protein sequences.
+  #
+  # Args:
+  #   file.name (type = string): File name of input fasta file
+  #   file.path (type = string): Path to where input file is located
+  #   job.name (type = string): Job name adds a time stamp to make them unique
+  #   version (type = string): ClustalW version
+  #   type (type = string): Either 'DNA' or 'PROTEIN'
+  #   args (type = string): Optional for arguments (i.e. flags, like on command 
+  #     line).  One string with commands separated by spaces. see help(ClustalW)
+  #     for details.
+  #   model (type = string): Name of substitution model.  For DNA the choices 
+  #     are GTRCAT, and JCCAT.  For protein the choices are JTTCAT, and WAGCAT.
+  #   gamma (type = boolean): Use this option (about 5\% slower) if you want 
+  #     to rescale the branch lengths and compute a Gamma20-based likelihood.
+  #   stat (type = boolean): This allows for statistical comparisons (when 
+  #     gamma=TRUE) of the likelihood of different topologies.
+  #   print.curl (type = string): Prints the associated curl statement
+  #   version (type = string): ClustalW version
+  #   shared.username (type = string): Valid iPlant username with whom the files 
+  #     are being shared.
+  #   suppress.Warnings (type = boolean): Don't do any error checking
+  #   email (type = boolean): By default an email will be sent to the user when 
+  #     the job finishes.
+
+  # Returns:
+  #   Returns the job id (number).  o/w an error
+
   type <- match.arg(type, c("DNA", "PROTEIN"))
 
   if (type == "DNA"){
@@ -14,7 +43,8 @@ Fasttree <- function(file.name, file.path="", job.name=NULL, args=NULL,
     model <- match.arg(model, c("GTRCAT", "JCCAT"))
 
     if (is.null(job.name)){
-      job.name <- paste(rplant.env$user, "_Fasttreedna_", model, "_viaR", sep="")
+      job.name <- paste(rplant.env$user, "_Fasttreedna_", model, "_viaR",
+                        sep="")
     }
 
   } else {
@@ -26,7 +56,8 @@ Fasttree <- function(file.name, file.path="", job.name=NULL, args=NULL,
     model <- match.arg(model, c("JTTCAT", "WAGCAT"))
 
     if (is.null(job.name)){
-      job.name <- paste(rplant.env$user, "_Fastreeprotein_", model, "_viaR", sep="")
+      job.name <- paste(rplant.env$user, "_Fastreeprotein_", model, "_viaR", 
+                        sep="")
     }
   }
 
